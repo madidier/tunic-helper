e3e158155fe1123a0144de94008944b5ea1b63b2
Maxime DIDIER <maxime.a.didier@gmail.com>
2022-03-28 22:01:09 +0200

    release build config for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const MonacoEditorPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/tunic-helper/' : '/',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
