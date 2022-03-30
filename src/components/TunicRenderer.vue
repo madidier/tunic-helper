aae5b1853f6ded265092d2bdb3d95ed83f2937ab
Maxime DIDIER <maxime.a.didier@gmail.com>
2022-03-30 21:46:56 +0200

    refactor TunicRenderer to composition API

--- src/components/TunicRenderer.vue
+++ src/components/TunicRenderer.vue
@@ -6,13 +6,13 @@
   -->
   <component v-if="render.container" :is="render.container">
     <TunicRenderer v-for="(child, index) of node.children"
-      :key="index" :node="child" :definitions="definitions" ref="children"
+      :key="index" :node="child" :definitions="definitions" :ref="e => children[index] = e"
       @change="c => updateChild(index, c)" />
   </component>
   <component v-else-if="render.single" :is="render.single" ref="self" />
   <span v-else-if="render.text" ref="self">{{ render.text }}</span>
   <TunicRenderer v-else-if="render.container === ''" v-for="(child, index) of node.children"
-    :key="index" :node="child" :definitions="definitions" ref="children"
+    :key="index" :node="child" :definitions="definitions" :ref="e => children[index] = e"
     @change="c => updateChild(index, c)" />
   <span v-else-if="render.glyphs" ref="self">
     <TunicWord :word="render.glyphs" :scale="hover ? 1.5 : 1"
@@ -28,146 +28,142 @@
   <span v-else-if="render.warn" class="warn" ref="self">{{ render.warn }}</span>
 </template>
 
-<script>
+<script setup>
+import { ref, computed } from 'vue'
 import TunicWord from './TunicWord.vue'
 
-export default {
-  components: {
-    TunicWord
-  },
-  props: ['node', 'definitions'],
-  emits: ['change'],
+/* global defineProps, defineEmits */
+const props = defineProps(['node', 'definitions'])
+const emit = defineEmits(['change'])
 
-  data () {
-    return {
-      hover: false
+const self = ref(null)
+// This should be an array managed by Vue but I seem to have found a bug
+// I should check if there is already a bug report
+const children = ref({})
+
+const hover = ref(false)
+
+const render = computed(() => {
+  // See https://github.com/syntax-tree/mdast
+  switch (props.node.type) {
+    case 'root':
+    case 'link': // Just skip links at least for now
+      return { container: '' }
+
+    case 'paragraph':
+      return { container: 'p' }
+
+    case 'heading': {
+      const depth = props.node.depth | 0
+      return { container: depth > 0 && depth <= 6 ? 'h' + depth : 'h2' }
     }
-  },
 
-  computed: {
-    render () {
-      // See https://github.com/syntax-tree/mdast
-      switch (this.node.type) {
-        case 'root':
-        case 'link': // Just skip links at least for now
-          return { container: '' }
+    case 'thematicBreak':
+      return { single: 'hr' }
 
-        case 'paragraph':
-          return { container: 'p' }
+    case 'blockquote':
+      return { container: 'blockquote' }
 
-        case 'heading': {
-          const depth = this.node.depth | 0
-          return { container: depth > 0 && depth <= 6 ? 'h' + depth : 'h2' }
-        }
+    case 'list':
+      return { container: props.node.ordered ? 'ol' : 'ul' }
 
-        case 'thematicBreak':
-          return { single: 'hr' }
+    case 'listItem':
+      return { container: 'li' }
 
-        case 'blockquote':
-          return { container: 'blockquote' }
+    case 'html':
+      if (props.node.value === '<br>') {
+        return { single: 'br' }
+      } else {
+        return { text: props.node.value }
+      }
 
-        case 'list':
-          return { container: this.node.ordered ? 'ol' : 'ul' }
+    case 'text':
+      return { text: props.node.value }
 
-        case 'listItem':
-          return { container: 'li' }
+    case 'code':
+      return { code: true }
 
-        case 'html':
-          if (this.node.value === '<br>') {
-            return { single: 'br' }
-          } else {
-            return { text: this.node.value }
-          }
+    case 'emphasis':
+      return { container: 'em' }
 
-        case 'text':
-          return { text: this.node.value }
+    case 'strong':
+      return { container: 'strong' }
 
-        case 'code':
-          return { code: true }
+    case 'inlineCode': {
+      const glyphs = props.node.value
+      const known = !hover.value && props.definitions[glyphs]
+      return known ? { known } : { glyphs }
+    }
 
-        case 'emphasis':
-          return { container: 'em' }
+    case 'break':
+      return { single: 'br' }
 
-        case 'strong':
-          return { container: 'strong' }
+    case 'definition':
+      return { defn: props.node.label, value: props.node.url }
 
-        case 'inlineCode': {
-          const glyphs = this.node.value
-          const known = !this.hover && this.definitions[glyphs]
-          return known ? { known } : { glyphs }
-        }
+    default:
+      return { warn: `Unsupported content (${props.node.type})` }
+  }
+})
 
-        case 'break':
-          return { single: 'br' }
+const updateValue = value => emit('change', { ...props.node, value })
+const updateChild = (index, value) => {
+  const children = props.node.children.slice()
+  children[index] = value
+  emit('change', { ...props.node, children })
+}
 
-        case 'definition':
-          return { defn: this.node.label, value: this.node.url }
-
-        default:
-          return { warn: `Unsupported content (${this.node.type})` }
+const scrollToLine = line => {
+  if (self.value) {
+    self.value.scrollIntoView()
+  } else {
+    // Assume ast nodes are ordered in a strictly ascending order
+    for (let i = 0; i < props.node.children.length; ++i) {
+      // First node whose end is past the target line gets selected
+      if (props.node.children[i].position.end.line >= line) {
+        children.value[i].scrollToLine(line)
+        return
       }
     }
-  },
-
-  methods: {
-    updateValue (value) {
-      this.$emit('change', { ...this.node, value })
-    },
-
-    updateChild (index, value) {
-      const children = this.node.children.slice()
-      children[index] = value
-      this.$emit('change', { ...this.node, children })
-    },
-
-    scrollToLine (line) {
-      if (this.$refs.self) {
-        this.$refs.self.scrollIntoView()
-      } else if (this.$refs.children) {
-        // Assume ast nodes are ordered in a strictly ascending order
-        for (let i = 0; i < this.$refs.children.length; ++i) {
-          // First node whose end is past the target line gets selected
-          if (this.node.children[i].position.end.line >= line) {
-            this.$refs.children[i].scrollToLine(line)
-            return
-          }
-        }
-        // Fallback to the last child
-        this.$refs.children[this.$refs.children.length - 1].scrollToLine(line)
-      }
-    },
-
-    // FIXME: This code is clumsy and might be unnecessarily inefficient
-    queryFirstLineVisibleIn (rect) {
-      if (this.$refs.self) {
-        return this.node.position.start.line
-      } else if (this.$refs.children) {
-        for (const child of this.$refs.children) {
-          if (child.isVisibleInRect(rect)) {
-            return child.queryFirstLineVisibleIn(rect)
-          }
-        }
-        // Fallback
-        return this.$refs.children[this.$refs.children.length - 1].queryFirstLineVisibleIn(rect)
-      }
-    },
-
-    isVisibleInRect (rect) {
-      if (this.$refs.self) {
-        const selfRect = this.$refs.self.getBoundingClientRect()
-        // Only the vertical axis check is really necessary, and we don't
-        // care if the element is not visible because it is past the end of
-        // the viewport.
-        return selfRect.bottom > rect.top
-      } else if (this.$refs.children) {
-        return this.$refs.children.some(child => child.isVisibleInRect(rect))
-      }
-
-      // Looks like we are not being rendered for some reason *shrug*
-      debugger
-    }
+    // Fallback to the last child
+    children.value[props.node.children.length - 1].scrollToLine(line)
   }
 }
+
+const queryFirstLineVisibleIn = rect => {
+  if (self.value) {
+    return props.node.position.start.line
+  } else {
+    for (let i = 0; i < props.node.children.length; ++i) {
+      const child = children.value[i]
+      if (child.isVisibleInRect(rect)) {
+        return child.queryFirstLineVisibleIn(rect)
+      }
+    }
+    // Fallback
+    return children.value[props.node.children.length - 1].queryFirstLineVisibleIn(rect)
+  }
+}
+
+const isVisibleInRect = rect => {
+  if (self.value) {
+    const selfRect = self.value.getBoundingClientRect()
+    // Only the vertical axis check is really necessary, and we don't
+    // care if the element is not visible because it is past the end of
+    // the viewport.
+    return selfRect.bottom > rect.top
+  } else {
+    for (let i = 0; i < props.node.children.length; ++i) {
+      if (children.value[i].isVisibleInRect(rect)) {
+        return true
+      }
+    }
+    return false
+  }
+}
+
+/* global defineExpose */
+defineExpose({ scrollToLine, queryFirstLineVisibleIn, isVisibleInRect })
 </script>
 
 <style scoped>
